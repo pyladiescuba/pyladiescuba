c7e31f5ca432b2d36a1e6f9c58a3a44cec47e5d2
Lily Perera <lilycapetillo86@gmail.com>
2020-02-06 21:02:54 +0000

    Updated .gitignore file and Added crisy-forms the settings proyect

--- pyladiescuba/settings.py
+++ pyladiescuba/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My App's
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
