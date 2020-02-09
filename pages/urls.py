524b4a70a1b6708c34bd011ebc6bc3b2b14ad8a6
Lily Perera <lilycapetillo86@gmail.com>
2020-02-09 21:12:05 +0000

    Testing the first data's

--- pages/urls.py
+++ pages/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from pages.views import home
+from django.urls import path
+from pages import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('pages/', include('pages.urls')),
+    path('', views.home , name='home')
 ]
