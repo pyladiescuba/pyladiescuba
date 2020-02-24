84eca4d3ca363249581f8a97bb76f0df1002959b
Lily Perera <lilycapetillo86@gmail.com>
2020-02-24 19:07:52 +0000

    Add route pages app and put strong to .html elements

--- pyladiescuba/urls.py
+++ pyladiescuba/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from pages.views import home
+from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls')),
+    path('', views.home, name='home'),
+
 ]
